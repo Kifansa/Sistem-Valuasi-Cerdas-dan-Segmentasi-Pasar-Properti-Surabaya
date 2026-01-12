8c4eac0e6ad1fa92062108ee20ffe70fbabd8f32
Alvyn Wira Pratama <168216543+alvynpratama@users.noreply.github.com>
2026-01-12 16:07:29 +0700

    Update app.py

--- DEPLOYMENT/app.py
+++ DEPLOYMENT/app.py
@@ -5,21 +5,17 @@ import os
 
 app = Flask(__name__)
 
-# SETUP PATH OTOMATIS
 base_dir = os.path.dirname(os.path.abspath(__file__))
 model_dir = os.path.join(base_dir, 'models')
 
-# LOAD MODEL & METADATA
 print("\n--------------------------------------------")
 print("STARTING SERVER")
 print("--------------------------------------------")
 
 try:
-    # Load Model Utama
     model_sup = joblib.load(os.path.join(model_dir, 'model_supervised.joblib'))
     model_unsup = joblib.load(os.path.join(model_dir, 'model_unsupervised.joblib'))
     
-    # Load Metadata
     meta_options = joblib.load(os.path.join(model_dir, 'meta_options.joblib'))
     
     print("Models & Metadata Loaded.")
@@ -43,9 +39,7 @@ def index():
     
     if request.method == 'POST':
         try:
-            # Capture Input User 
             input_data = {
-                # --- Numerik ---
                 'Luas Tanah': float(request.form['luas_tanah']),
                 'Luas Bangunan': float(request.form['luas_bangunan']),
                 'Kamar Tidur': int(request.form['kamar_tidur']),
@@ -53,7 +47,6 @@ def index():
                 'Daya Listrik': float(request.form['listrik']),
                 'Jumlah Lantai': float(request.form['jumlah_lantai']),
                 
-                # --- Kategorikal ---
                 'Kecamatan': request.form['kecamatan'],
                 'Sertifikat': request.form['sertifikat'],
                 'Kondisi Perabotan': request.form['perabotan'],
@@ -62,18 +55,14 @@ def index():
                 'Terjangkau Internet': request.form['internet']
             }
             
-            # Buat DataFrame
             df_input = pd.DataFrame([input_data])
 
-            # Supervised Prediction (Estimasi Harga)
             pred_harga_raw = model_sup.predict(df_input)[0]
             pred_harga = f"Rp {pred_harga_raw:,.0f}".replace(',', '.')
 
-            # Unsupervised Prediction (Clustering)
             df_cluster_input = df_input[['Luas Tanah', 'Luas Bangunan', 'Kamar Tidur', 'Kamar Mandi']]
             cluster_id = model_unsup.predict(df_cluster_input)[0]
 
-            # Interpretasi Bisnis
             cluster_map = {
                 0: {'title': 'Compact / Entry-Level Asset', 
                     'desc': 'Properti efisien dengan luas terbatas. Cocok untuk keluarga baru atau investasi awal.',
@@ -87,7 +76,6 @@ def index():
             }
             cluster_info = cluster_map.get(cluster_id, cluster_map[0])
 
-            # Update Context untuk dikirim ke UI
             context.update({
                 'result_exists': True,
                 'price': pred_harga,
